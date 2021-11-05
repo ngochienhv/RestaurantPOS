35754f04830f1c69dcab9a2492cc09fb52ddb883
ggnuDD <93441263+ggnuDD@users.noreply.github.com>
2021-11-05 20:00:04 +0700

    Update mcdonalds.js

--- src/mcdonalds.js
+++ src/mcdonalds.js
@@ -1,50 +1,370 @@
-//img = id
+//img =     ID
 export const foodArray = [
-    {ID:"1", name:"Fried Chicken", type:"Fried Chicken", price:36000, src:"https://mcdonalds.vn/uploads/2018/food/ga-ran/1-ga-ran.png", count: 0, total: 0},
-    {ID:"2", name:"Fried Chicken - 3 PCs", type:["Fried Chicken","Set"], price:99000, src:"https://mcdonalds.vn/uploads/2018/food/ga-ran/3-ga-ran.png", count: 0, total: 0},
-    {ID:"3", name:"Fried Chicken - 9 PCs", type:["Fried Chicken","Set"], price:289000, src:"https://mcdonalds.vn/uploads/2018/food/ga-ran/9-ga-ran.png", count: 0, total: 0},
-    {ID:"4", name:"Fried Chicken Meal - 2 PCs", type:["Fried Chicken", "Set"], price:86000, src:"https://mcdonalds.vn/uploads/2018/2-ga-ran.png", count: 0, total: 0},
-    {ID:"5", name:"Chicken Nuggets - 4 PCs", type:["Fried Chicken", "Set"], price:36000, src:"https://mcdonalds.vn/uploads/2018/4nuggets.png", count: 0, total: 0},
-    {ID:"6", name:"Chicken Nuggets - 9 PCs", type:["Fried Chicken", "Set"], price:69000, src:"https://mcdonalds.vn/uploads/2018/food/ga-ran/9pcs-chicken-mcnuggets.png", count: 0, total: 0},
-
-    {ID:"7", name:"Burger Bulgogi", type:["Burger", "Beef"], price:79000, src:"https://mcdonalds.vn/uploads/2018/mcroyal_deluxe.png", count: 0, total: 0},
-    {ID:"8", name:"Cheeseburger", type:["Burger", "Beef"], price:49000, src:"https://mcdonalds.vn/uploads/2018/food/burgers/cheese-burger-deluxe.png", count: 0, total: 0},
-    {ID:"9", name:"Double Cheeseburger", type:["Burger", "Beef"], price:49000, src:"https://mcdonalds.vn/uploads/2018/food/burgers/double-cheese-burger.png", count: 0, total: 0},
-    {ID:"10", name:"Chicken Burger", type:["Burger","Chicken"], price:32000, src:"https://mcdonalds.vn/uploads/2018/food/mon-an-chung/chicken_burger.png", count: 0, total: 0},
-    {ID:"11", name:"Double Chicken Burger", type:["Burger","Chicken"], price:79000, src:"https://mcdonalds.vn/uploads/2018/food/burgers/mcspicy-deluxe.png", count: 0, total: 0},
-    {ID:"12", name:"Fish Burger", type:["Burger","Fish"], price:49000, src:"https://mcdonalds.vn/uploads/2018/food-categories/CSO_1063.png", count: 0, total: 0},
-    {ID:"13", name:"Pork Burger", type:["Burger","Pork"], price:32000, src:"https://mcdonalds.vn/uploads/2018/food/mon-an-chung/pork_burger.png", count: 0, total: 0},
+    {
+        ID:"1", name:"Fried Chicken", 
+        type:["Fried Chicken"],
+        price:36000,
+        src:"https://mcdonalds.vn/uploads/2018/food/ga-ran/1-ga-ran.png",
+        count: 0,
+        total: 0,
+        detail:["1 Miếng Gà Giòn"]
+    },
+    {
+        ID:"2", name:"Fried Chicken - 3 PCs", 
+        type:["Fried Chicken","Set"],
+        price:99000,
+        src:"https://mcdonalds.vn/uploads/2018/food/ga-ran/3-ga-ran.png",
+        count: 0,
+        total: 0,
+        detail:["3 Miếng Gà Giòn"]
+    },
+    {
+        ID:"3", name:"Fried Chicken - 9 PCs", 
+        type:["Fried Chicken","Set"],
+        price:289000,
+        src:"https://mcdonalds.vn/uploads/2018/food/ga-ran/9-ga-ran.png",
+        count: 0,
+        total: 0,
+        detail:["9 Miếng Gà Giòn"]
+    },
+    {
+        ID:"4", name:"Fried Chicken Meal - 2 PCs", 
+        type:["Fried Chicken", "Set"],
+        price:86000,
+        src:"https://mcdonalds.vn/uploads/2018/2-ga-ran.png",
+        count: 0,
+        total: 0,
+        detail:["2 Miếng Gà Giòn","1 Khoai Tây Nhỏ", "1 Coca - M"]
+    },
+    {
+        ID:"5", name:"Chicken Nuggets - 4 PCs", 
+        type:["Fried Chicken", "Set"],
+        price:36000,
+        src:"https://mcdonalds.vn/uploads/2018/4nuggets.png",
+        count: 0,
+        total: 0,
+        detail:["4 Miếng Gà viên"]
+    },
+    {
+        ID:"6", name:"Chicken Nuggets - 9 PCs", 
+        type:["Fried Chicken", "Set"],
+        price:69000,
+        src:"https://mcdonalds.vn/uploads/2018/food/ga-ran/9pcs-chicken-mcnuggets.png",
+        count: 0,
+        total: 0,
+        detail:["9 Miếng Gà viên"]
+    },
     
-    {ID:"14", name:"Grilled Pork Rice", type:["Meal","Pork"], price:46000, src:"https://mcdonalds.vn/uploads/2018/food/rice/MEAL_porkrice.png", count: 0, total: 0},
-    {ID:"15", name:"Grilled Pork Rice with Egg", type:["Meal","Pork"], price:66000, src:"https://mcdonalds.vn/uploads/2018/food/rice/MEAL_porkeggrice.png", count: 0, total: 0},
-    {ID:"16", name:"Chicken Rice", type:["Meal","Chicken"], price:46000, src:"https://mcdonalds.vn/uploads/2018/food/rice/MEAL_chickrice.png", count: 0, total: 0},
-    {ID:"17", name:"Spicy Sauce Chicken Rice", type:["Meal","Chicken"], price:90000, src:"https://mcdonalds.vn/uploads/2018/food/rice/MEAL_mcspicyrice.png", count: 0, total: 0},
-    {ID:"18", name:"Fried Chicken Rice", type:["Meal","Fried Chicken"], price:42000, src:"https://mcdonalds.vn/uploads/2018/food-categories/com-ga.png", count: 0, total: 0},
+    {
+        ID:"7", name:"Burger Bulgogi", 
+        type:["Burger", "Beef"],
+        price:79000,
+        src:"https://mcdonalds.vn/uploads/2018/mcroyal_deluxe.png",
+        count: 0,
+        total: 0,
+        detail:["1 Burger Bò"]
+    },
+    {
+        ID:"8", name:"Cheeseburger", 
+        type:["Burger", "Beef"],
+        price:49000,
+        src:"https://mcdonalds.vn/uploads/2018/food/burgers/cheese-burger-deluxe.png",
+        count: 0,
+        total: 0,
+        detail:["1 Burger Phô Mai"]
+    },
+    {
+        ID:"9", name:"Double Cheeseburger", 
+        type:["Burger", "Beef"],
+        price:49000,
+        src:"https://mcdonalds.vn/uploads/2018/food/burgers/double-cheese-burger.png",
+        count: 0,
+        total: 0,
+        detail:["1 Burger Phô Mai 2 Tầng"]
+    },
+    {
+        ID:"10", name:"Chicken Burger", 
+        type:["Burger","Chicken"],
+        price:32000,
+        src:"https://mcdonalds.vn/uploads/2018/food/mon-an-chung/chicken_burger.png",
+        count: 0,
+        total: 0,
+        detail:["1 Burger Gà"]
+    },
+    {
+        ID:"11", name:"Double Chicken Burger", 
+        type:["Burger","Chicken"],
+        price:79000,
+        src:"https://mcdonalds.vn/uploads/2018/food/burgers/mcspicy-deluxe.png",
+        count: 0,
+        total: 0,
+        detail:["1 Burger Gà 2 Tầng"]
+    },
+    {
+        ID:"12", name:"Fish Burger", 
+        type:["Burger","Fish"],
+        price:49000,
+        src:"https://mcdonalds.vn/uploads/2018/food-categories/CSO_1063.png",
+        count: 0,
+        total: 0,
+        detail:["1 Burger Cá"]
+    },
+    {
+        ID:"13", name:"Pork Burger", 
+        type:["Burger","Pork"],
+        price:32000,
+        src:"https://mcdonalds.vn/uploads/2018/food/mon-an-chung/pork_burger.png",
+        count: 0,
+        total: 0,
+        detail:["1 Burger Heo"]
+    },
+    
+    {
+        ID:"14", name:"Grilled Pork Rice", 
+        type:["Meal","Pork"],
+        price:46000,
+        src:"https://mcdonalds.vn/uploads/2018/food/rice/MEAL_porkrice.png",
+        count: 0,
+        total: 0,
+        detail:["1 Phần Cơm Thịt Heo","1 Coca - M"]
+    },
+    {
+        ID:"15", name:"Grilled Pork Rice with Egg", 
+        type:["Meal","Pork"],
+        price:66000,
+        src:"https://mcdonalds.vn/uploads/2018/food/rice/MEAL_porkeggrice.png",
+        count: 0,
+        total: 0,
+        detail:["1 Phần Cơm Thịt Heo 1 Trứng", "1 Coca L"]
+    },
+    {
+        ID:"16", name:"Chicken Rice", 
+        type:["Meal","Chicken"],
+        price:46000,
+        src:"https://mcdonalds.vn/uploads/2018/food/rice/MEAL_chickrice.png",
+        count: 0,
+        total: 0,
+        detail:["1 Phần Cơm Gà Rán", "1 Coca - M"]
+    },
+    {
+        ID:"17", name:"Spicy Sauce Chicken Rice", 
+        type:["Meal","Chicken"],
+        price:90000,
+        src:"https://mcdonalds.vn/uploads/2018/food/rice/MEAL_mcspicyrice.png",
+        count: 0,
+        total: 0,
+        detail:["1 Phần Cơm Gà Rán Sốt Cay", "1 Coca - L"]
+    },
+    {
+        ID:"18", name:"Fried Chicken Rice", 
+        type:["Meal","Fried Chicken"],
+        price:42000,
+        src:"https://mcdonalds.vn/uploads/2018/food-categories/com-ga.png",
+        count: 0,
+        total: 0,
+        detail:["1 Phần Cơm", "1 Miếng Gà Giòn"]
+    },
 
-    {ID:"19", name:"Sprite", type:"Beverage", price:20000, src:"https://mcdonalds.vn/uploads/2018/food/beverage/Product_thumb_Sprite.png", count: 0, total: 0},
-    {ID:"20", name:"Coca-Cola", type:"Beverage", price:20000, src:"https://mcdonalds.vn/uploads/2018/food/beverage/mcd-food-beverages-soft-drinks-coke.png", count: 0, total: 0},
-    {ID:"21", name:"Lychee Black Tea", type:"Beverage", price:45000, src:"https://mcdonalds.vn/uploads/2018/lychee.png", count: 0, total: 0},
-    {ID:"22", name:"Fanta", type:"Beverage", price:20000, src:"https://mcdonalds.vn/uploads/2018/food/beverage/hero-pdt-Fanta-201703_0.png", count: 0, total: 0},
-    {ID:"23", name:"Dasani Water", type:"Beverage", price:20000, src:"https://mcdonalds.vn/uploads/2018/food/beverage/dasani_water.png", count: 0, total: 0},
-    {ID:"24", name:"Sprite - L", type:"Beverage", price:20000, src:"https://mcdonalds.vn/uploads/2018/food/beverage/Product_thumb_Sprite.png", count: 0, total: 0},
-    {ID:"25", name:"Coca-Cola - L", type:"Beverage", price:20000, src:"https://mcdonalds.vn/uploads/2018/food/beverage/mcd-food-beverages-soft-drinks-coke.png", count: 0, total: 0},
-    {ID:"26", name:"Fanta - L", type:"Beverage", price:20000, src:"https://mcdonalds.vn/uploads/2018/food/beverage/hero-pdt-Fanta-201703_0.png", count: 0, total: 0},
+    {
+        ID:"19", name:"Sprite", 
+        type:["Beverage"],
+        price:20000,
+        src:"https://mcdonalds.vn/uploads/2018/food/beverage/Product_thumb_Sprite.png",
+        count: 0,
+        total: 0,
+        detail:["Nước Ngọt Có Ga Sprite - M"]
+    },
+    {
+        ID:"20", name:"Coca-Cola", 
+        type:["Beverage"],
+        price:20000,
+        src:"https://mcdonalds.vn/uploads/2018/food/beverage/mcd-food-beverages-soft-drinks-coke.png",
+        count: 0,
+        total: 0,
+        detail:["Nước Ngọt Có Ga Coca - M"]
+    },
+    {
+        ID:"21", name:"Lychee Black Tea", 
+        type:["Beverage"],
+        price:45000,
+        src:"https://mcdonalds.vn/uploads/2018/lychee.png",
+        count: 0,
+        total: 0,
+        detail:["Hồng Trà Vải"]
+    },
+    {
+        ID:"22", name:"Fanta", 
+        type:["Beverage"],
+        price:20000,
+        src:"https://mcdonalds.vn/uploads/2018/food/beverage/hero-pdt-Fanta-201703_0.png",
+        count: 0,
+        total: 0,
+        detail:["Nước Ngọt Có Ga Fanta - M"]
+    },
+    {
+        ID:"23", name:"Dasani Water", 
+        type:["Beverage"],
+        price:20000,
+        src:"https://mcdonalds.vn/uploads/2018/food/beverage/dasani_water.png",
+        count: 0,
+        total: 0,
+        detail:["Nước Tinh Khiết Dasani"]
+    },
+    {
+        ID:"24", name:"Sprite - L", 
+        type:["Beverage"],
+        price:20000,
+        src:"https://mcdonalds.vn/uploads/2018/food/beverage/Product_thumb_Sprite.png",
+        count: 0,
+        total: 0,
+        detail:["Nước Ngọt Có Ga Sprite - L"]
+    },
+    {
+        ID:"25", name:"Coca-Cola - L", 
+        type:["Beverage"],
+        price:20000,
+        src:"https://mcdonalds.vn/uploads/2018/food/beverage/mcd-food-beverages-soft-drinks-coke.png",
+        count: 0,
+        total: 0,
+        detail:["Nước Ngọt Có Ga Coca - L"]
+    },
+    {
+        ID:"26", name:"Fanta - L", 
+        type:["Beverage"],
+        price:20000,
+        src:"https://mcdonalds.vn/uploads/2018/food/beverage/hero-pdt-Fanta-201703_0.png",
+        count: 0,
+        total: 0,
+        detail:["Nước Ngọt Có Ga Fanta - L"]
+    },
 
-    {ID:"27", name:"Vanilla Cone", type:"Desert", price:10000, src:"https://mcdonalds.vn/uploads/2018/food/desserts/mcdonalds_cone.png", count: 0, total: 0},
-    {ID:"28", name:"Apple Pie", type:["Desert","Snacks"], price:25000, src:"https://mcdonalds.vn/uploads/2018/food/desserts/apple-hotpie.png", count: 0, total: 0},
-    {ID:"29", name:"Chocolate Sundae", type:"Desert", price:29000, src:"https://mcdonalds.vn/uploads/2018/food/desserts/hotfudge_mcsundae.png", count: 0, total: 0},
-    {ID:"30", name:"Strawberry Sundae", type:"Desert", price:29000, src:"https://mcdonalds.vn/uploads/2018/food/desserts/strawberry-mcsundae.png", count: 0, total: 0},
-    {ID:"31", name:"Oreo Flurry", type:"Desert", price:36000, src:"https://mcdonalds.vn/uploads/2018/food/desserts/oreo_mcflurrt.png", count: 0, total: 0},
+    {
+        ID:"27", name:"Vanilla Cone", 
+        type:["Desert"],
+        price:10000,
+        src:"https://mcdonalds.vn/uploads/2018/food/desserts/mcdonalds_cone.png",
+        count: 0,
+        total: 0,
+        detail:["Kem Ốc Quế Vani"]
+    },
+    {
+        ID:"28", name:"Apple Pie", 
+        type:["Desert","Snacks"],
+        price:25000,
+        src:"https://mcdonalds.vn/uploads/2018/food/desserts/apple-hotpie.png",
+        count: 0,
+        total: 0,
+        detail:["Bánh Táo"]
+    },
+    {
+        ID:"29", name:"Chocolate Sundae", 
+        type:["Desert"],
+        price:29000,
+        src:"https://mcdonalds.vn/uploads/2018/food/desserts/hotfudge_mcsundae.png",
+        count: 0,
+        total: 0,
+        detail:["Kem Ly Sôcôla"]
+    },
+    {
+        ID:"30", name:"Strawberry Sundae", 
+        type:["Desert"],
+        price:29000,
+        src:"https://mcdonalds.vn/uploads/2018/food/desserts/strawberry-mcsundae.png",
+        count: 0,
+        total: 0,
+        detail:["Kem Ly Dâu"]
+    },
+    {
+        ID:"31", name:"Oreo Flurry", 
+        type:["Desert"],
+        price:36000,
+        src:"https://mcdonalds.vn/uploads/2018/food/desserts/oreo_mcflurrt.png",
+        count: 0,
+        total: 0,
+        detail:["Kem Oreo"]
+    },
 
-    {ID:"32", name:"French Fries - S", type:"Snacks", price:16000, src:"https://mcdonalds.vn/uploads/2018/food/ga-ran/regular_world_famous_fries.png", count: 0, total: 0},
-    {ID:"33", name:"French Fries - M", type:"Snacks", price:26000, src:"https://mcdonalds.vn/uploads/2018/food/ga-ran/medium_world_famous_fries.png", count: 0, total: 0},
-    {ID:"34", name:"French Fries - L", type:"Snacks", price:36000, src:"https://mcdonalds.vn/uploads/2018/food/ga-ran/large_world_famous_fries.png", count: 0, total: 0},
+    {
+        ID:"32", name:"French Fries - S", 
+        type:["Snacks"],
+        price:16000,
+        src:"https://mcdonalds.vn/uploads/2018/food/ga-ran/regular_world_famous_fries.png",
+        count: 0,
+        total: 0,
+        detail:["1 Khoai Tây Chiên - S"]
+    },
+    {
+        ID:"33", name:"French Fries - M", 
+        type:["Snacks"],
+        price:26000,
+        src:"https://mcdonalds.vn/uploads/2018/food/ga-ran/medium_world_famous_fries.png",
+        count: 0,
+        total: 0,
+        detail:["1 Khoai Tây Chiên - M"]
+    },
+    {
+        ID:"34", name:"French Fries - L", 
+        type:["Snacks"],
+        price:36000,
+        src:"https://mcdonalds.vn/uploads/2018/food/ga-ran/large_world_famous_fries.png",
+        count: 0,
+        total: 0,
+        detail:["1 Khoai Tây Chiên - L"]
+    },
 
-    {ID:"35", name:"Combo 1", type:"Combo", price:81000, src:"https://mcdonalds.vn/uploads/2018/food/Combo/com79k.png", count: 0, total: 0},
-    {ID:"36", name:"Combo 2", type:"Combo", price:129000, src:"https://mcdonalds.vn/uploads/2018/food/Combo/combo2A.png", count: 0, total: 0},
-    {ID:"37", name:"Combo 3", type:"Combo", price:169000, src:"https://mcdonalds.vn/uploads/2018/food/Combo/combo2B.png", count: 0, total: 0},
-    {ID:"38", name:"Combo 4", type:"Combo", price:179000, src:"https://mcdonalds.vn/uploads/2018/food/Combo/combo2C.png", count: 0, total: 0},
-    {ID:"39", name:"Combo 5", type:"Combo", price:269000, src:"https://mcdonalds.vn/uploads/2018/food/Combo/combo3A.png", count: 0, total: 0},
-    {ID:"40", name:"Combo 6", type:"Combo", price:279000, src:"https://mcdonalds.vn/uploads/2018/food/Combo/combo3B.png", count: 0, total: 0},
+    {
+        ID:"35", name:"Combo 1", 
+        type:["Combo"],
+        price:81000,
+        src:"https://mcdonalds.vn/uploads/2018/food/Combo/com79k.png",
+        count: 0,
+        total: 0,
+        detail:["1 Miếng Gà Giòn", "1 Burger Phô Mai", "1 Coca - L"]
+    },
+    {
+        ID:"36", name:"Combo 2", 
+        type:["Combo"],
+        price:129000,
+        src:"https://mcdonalds.vn/uploads/2018/food/Combo/combo2A.png",
+        count: 0,
+        total: 0,
+        detail:["2 Miếng Gà Giòn", "1 Burger Phô Mai 2 Tầng", "2 Coca - L"]
+    },
+    {
+        ID:"37", name:"Combo 3", 
+        type:["Combo"],
+        price:169000,
+        src:"https://mcdonalds.vn/uploads/2018/food/Combo/combo2B.png",
+        count: 0,
+        total: 0,
+        detail:["3 Miếng Gà Giòn", "4 Miếng Gà Viên", "1 Khoai Tây Chiên - M", "2 Coca - L"]
+    },
+    {
+        ID:"38", name:"Combo 4", 
+        type:["Combo"],
+        price:179000,
+        src:"https://mcdonalds.vn/uploads/2018/food/Combo/combo2C.png",
+        count: 0,
+        total: 0,
+        detail:["2 Miếng Gà Giòn", "1 Burger Phô Mai", "1 Burger Phô Mai 2 Tầng" , "1 Khoai Tây Chiên - M", "2 Coca - L"]
+    },
+    {
+        ID:"39", name:"Combo 5", 
+        type:["Combo"],
+        price:269000,
+        src:"https://mcdonalds.vn/uploads/2018/food/Combo/combo3A.png",
+        count: 0,
+        total: 0,
+        detail:["5 Miếng Gà Giòn", "6 Miếng Gà Viên", "2 Khoai Tây Chiên - M", "3 Coca - L"]
+    },
+    {
+        ID:"40", name:"Combo 6", 
+        type:["Combo"],
+        price:279000,
+        src:"https://mcdonalds.vn/uploads/2018/food/Combo/combo3B.png",
+        count: 0,
+        total: 0,
+        detail:["4 Miếng Gà Giòn", "1 Burger Phô Mai", "1 Burger Phô Mai 2 Tầng" , "2 Khoai Tây Chiên - M", "3 Coca - L"]
+    },
 ];
 
