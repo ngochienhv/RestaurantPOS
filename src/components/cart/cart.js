624bdec3209a44cd33e81fa5cb39bbf16198231e
Elvis Nhat <hongnhat195>
2021-11-06 10:22:35 +0700

    nhat

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -4,42 +4,54 @@ import CartItem from "./cartItem";
 import { CartContainer } from "../../container/CartContainer";
 import priceWithDots from "../products/priceWithDots";
 export default class Cart extends React.Component {
-    render() {
-        return (
-            <CartContainer>
-            <div className="container cart position-fixed">
-                <Consumer>
-                    {value => {
-                        const { cart } = value;
-                        if (cart.length > 0) {
-                            return (
-                                <React.Fragment>
-                                    <div className="row cart-row">
-                                        <div className="col-sm-12 col-md-12 col-lg-12 cart-col">
-                                        <p className="cart-title"><i class="fas fa-shopping-cart"></i> Your cart</p>     
-                                    {value.cart.map(product => { return <CartItem key={product.ID} product={product} /> })}
-                                    </div>
-                                </div>
-                                    <div className="total-container">
-                                            <div className="d-flex justify-content-between">
-                                                <p className="text-left p1">Total: </p>
-                                                <p className="text-right p2"><strong>{priceWithDots(value.cartTotal)} VND</strong></p>
-                                            </div>
-                                        <button className="btn btn-primary btn-md clear-btn" onClick={() => value.clearCart()}>Clear</button>
-                                    </div>
-                                    <button className="btn btn-primary btn-lg payment-btn">Payment</button>
-                                </React.Fragment>
-                            )
-                        }
-                        else {
-                            return (
-                                <h3>Your cart is empty</h3>
-                            )
-                        }
-                    }}
-                    </Consumer>
-                </div>
-            </CartContainer>
-        )
-    }
+  render() {
+    return (
+      <CartContainer>
+        <div className="container cart position-fixed">
+          <Consumer>
+            {(value) => {
+              const { cart } = value;
+              if (cart.length > 0) {
+                return (
+                  <React.Fragment>
+                    <div className="row cart-row">
+                      <div className="col-sm-12 col-md-12 col-lg-12 cart-col">
+                        <p className="cart-title">
+                          <i class="fas fa-shopping-cart"></i> Your cart
+                        </p>
+                        {value.cart.map((product) => {
+                          return (
+                            <CartItem key={product.ID} product={product} />
+                          );
+                        })}
+                      </div>
+                    </div>
+                    <div className="total-container">
+                      <div className="d-flex justify-content-between">
+                        <p className="text-left p1">Total: </p>
+                        <p className="text-right p2">
+                          <strong>{priceWithDots(value.cartTotal)} VND</strong>
+                        </p>
+                      </div>
+                      <button
+                        className="btn btn-primary btn-md clear-btn"
+                        onClick={() => value.clearCart()}
+                      >
+                        Clear
+                      </button>
+                    </div>
+                    <button className="btn btn-primary btn-lg payment-btn">
+                      Payment
+                    </button>
+                  </React.Fragment>
+                );
+              } else {
+                return <h3>Your cart is empty</h3>;
+              }
+            }}
+          </Consumer>
+        </div>
+      </CartContainer>
+    );
+  }
 }
---
+++ src/foodType.js
@@ -0,0 +1,47 @@
+const foodType = [
+  {
+    name: "Beverage",
+    src: "https://mcdonalds.vn/uploads/2018/food-categories/thuc-uong.png",
+  },
+  {
+    name: "Desert",
+    src: "https://mcdonalds.vn/uploads/2018/food-categories/deserts.png",
+  },
+  {
+    name: "Snacks",
+    src: "https://mcdonalds.vn/uploads/2018/food-categories/monannhe.png",
+  },
+  {
+    name: "Fried Chicken",
+    src: "https://mcdonalds.vn/uploads/2018/food-categories/ga-ran.png",
+  },
+  {
+    name: "Set",
+    src: "https://mcdonalds.vn/uploads/2018/food-categories/breakfast-EVM.png",
+  },
+  {
+    name: "Burger",
+    src: "https://mcdonalds.vn/uploads/2018/food-categories/burgers.png",
+  },
+  {
+    name: "Beef",
+    src: "https://mcdonalds.vn/uploads/2018/food-categories/mcsaver.png",
+  },
+  {
+    name: "Chicken",
+    src: "https://mcdonalds.vn/uploads/2018/food/ga-ran/1-ga-ran.png",
+  },
+  {
+    name: "Fish",
+    src: "https://mcdonalds.vn/uploads/2018/food-categories/diem-tam-muffin.png",
+  },
+  {
+    name: "Pork",
+    src: "https://mcdonalds.vn/uploads/2018/food-categories/com.png",
+  },
+  {
+    name: "Combo",
+    src: "https://mcdonalds.vn/uploads/2018/food-categories/cate.png",
+  },
+];
+export default foodType;
